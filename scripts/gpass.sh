f046c27ed5a4ac30b1cc95424105b6bfe8b5765a
Shakiba Moshiri <net@shakiba.net>
2023-02-07 08:06:28 +0330

    Update gpass.sh

--- scripts/gpass.sh
+++ scripts/gpass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tr -dc [:lower:][:digit:] < /dev/urandom | head -c 8; echo
-tr -dc [:lower:][:digit:][:upper:] < /dev/urandom | head -c 16; echo
+tr -dc [:lower:][:digit:] < /dev/urandom | head -c 6; echo
+tr -dc [:lower:][:digit:][:upper:] < /dev/urandom | head -c 12; echo
